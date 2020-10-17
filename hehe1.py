4655dc5df1e38bd78ae1d6073dbebaec4670ffbc
Brian Lee <liletian.us@gmail.com>
2020-10-17 00:10:04 -0700

    add code

---
+++ hehe1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+date_string = os.popen("ls -l").read()
+#print(date_string)
+
+i=0
+y=""
+lst=[]
+for x in date_string:
+    if (x!="\n"):
+        y=y+x
+    else:
+#        print(y)
+        if(i>0):
+            y=y.split()
+            lst.append(y)
+            print("kill -9 " + y[1])
+        y=""
+        i=i+1
+print(i)
+
+ii=0
+
+for ii in range(len(lst)):
+    print("kill -9 " + lst[ii][1])
+    ii=ii+1
